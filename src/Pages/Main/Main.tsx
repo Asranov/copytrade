ddb1f4ec9b2751746797ec8d258af1bfd8ac8aa7
Asranov <asranov0003@gmail.com>
2023-03-04 18:32:43 +0500

    top traders added

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
-type Props = {}
-
-export default function Main({ }: Props) {
+export default function Main() {
   return (
     <>
       <header id='header' className='fixed-top'>
@@ -79,33 +77,240 @@ export default function Main({ }: Props) {
 
       </section>
 
+      <main id='main'>
 
 
-      <section id="about" className="about section-bg">
-        <div className="container aos-init aos-animate" data-aos="fade-up">
+
+        <section id="about" className="about section-bg">
+          <div className="container aos-init aos-animate" data-aos="fade-up">
 
 
-          <div className="section-title">
-            <h2>О нас</h2>
+            <div className="section-title">
+              <h2>О нас</h2>
+            </div>
+
+            <div>
+              <p >Сегодня Binance, Huobi, Coinbase и Bybit признаны самыми популярными платформами для торговли фьючерсами. Торговля на фьючерсах binance, Huobi, Coinbase и Bybit позволяют всем трейдерам ежедневно получать прибыль от различных сделок. На платформе предусмотрена спотовая и фьючерсная торговля. Однако быстро и полностью разобраться во всех условиях, показателях криптовалютного рынка способен не каждый инвестор.</p>
+              <p >Наш сервис по копитрейдингу готов каждого желающего научить, как заработать на фьючерсах binance, Huobi, Coinbase и Bybit. Мы предлагаем воспользоваться услугами опытных и успешных трейдеров, которые уже долгие годы занимаются сделками на криптобиржах, прекрасно разбираются в тенденциях рынка, учитывают при совершении торговых операций многие факторы.</p>
+              <p >С нашей помощью вам уже не будут нужны торговые роботы для криптовалюты. Все действия и транзакции на нашем сервисе осуществляют живые аналитики. Мы настоятельно рекомендуем начинающим торговцам не делать ставку на специальные программы, а довериться экспертам.</p>
+              <p >Сотрудничество с нашим сервисом позволит вам получить следующие преимущества:</p>
+              <p >1. Отсутствие необходимости периодически вносить настройки в программу, без которых бот для торговли на бирже криптовалют не сможет приносить прибыль, заключать успешные сделки.</p>
+              <p >2. Получение только проверенных сигналов от наших трейдеров, которые всегда учитывают волотильность рынка, возникшие нюансы.</p>
+              <p >3. Постоянная подписка на профессионального трейдера – залог не только стабильного дохода, но и экономия личного времени, сохранение и преувеличение вложенных денег.</p>
+              <p >4. Сотрудничество с опытными торговцами, которые знают, как заработать на фьючерсах binance стратегия – отсутствие рисков, возникающих при личном совершении сделок.</p>
+              <p >5. Есть возможность совершить подписку сразу на несколько трейдеров, что позволит значительно увеличить прибыль.</p>
+              <p >6. Каждый наш аналитик не только знает, что такое фьючерсы бинанс стратегия, но и регулярно ее улучшает с учетом динамики рынка, новых на нем тенденций. Существующие сегодня криптоботы не способны так быстро адаптироваться под современные требования, торгуют по установленным настройкам.</p>
+              <p >7. Для выбора трейдера есть полная статистика сделок, история торгов.</p>
+              <p >Сотрудничество с нашим сервисом по криптотрейдингу позволит вам получать только проверенные сигналы для сделок, организовать постоянный пассивный доход, изменить жизнь к лучшему.</p>
+            </div>
+
           </div>
+        </section >
 
-          <div>
-            <p >Сегодня Binance, Huobi, Coinbase и Bybit признаны самыми популярными платформами для торговли фьючерсами. Торговля на фьючерсах binance, Huobi, Coinbase и Bybit позволяют всем трейдерам ежедневно получать прибыль от различных сделок. На платформе предусмотрена спотовая и фьючерсная торговля. Однако быстро и полностью разобраться во всех условиях, показателях криптовалютного рынка способен не каждый инвестор.</p>
-            <p >Наш сервис по копитрейдингу готов каждого желающего научить, как заработать на фьючерсах binance, Huobi, Coinbase и Bybit. Мы предлагаем воспользоваться услугами опытных и успешных трейдеров, которые уже долгие годы занимаются сделками на криптобиржах, прекрасно разбираются в тенденциях рынка, учитывают при совершении торговых операций многие факторы.</p>
-            <p >С нашей помощью вам уже не будут нужны торговые роботы для криптовалюты. Все действия и транзакции на нашем сервисе осуществляют живые аналитики. Мы настоятельно рекомендуем начинающим торговцам не делать ставку на специальные программы, а довериться экспертам.</p>
-            <p >Сотрудничество с нашим сервисом позволит вам получить следующие преимущества:</p>
-            <p >1. Отсутствие необходимости периодически вносить настройки в программу, без которых бот для торговли на бирже криптовалют не сможет приносить прибыль, заключать успешные сделки.</p>
-            <p >2. Получение только проверенных сигналов от наших трейдеров, которые всегда учитывают волотильность рынка, возникшие нюансы.</p>
-            <p >3. Постоянная подписка на профессионального трейдера – залог не только стабильного дохода, но и экономия личного времени, сохранение и преувеличение вложенных денег.</p>
-            <p >4. Сотрудничество с опытными торговцами, которые знают, как заработать на фьючерсах binance стратегия – отсутствие рисков, возникающих при личном совершении сделок.</p>
-            <p >5. Есть возможность совершить подписку сразу на несколько трейдеров, что позволит значительно увеличить прибыль.</p>
-            <p >6. Каждый наш аналитик не только знает, что такое фьючерсы бинанс стратегия, но и регулярно ее улучшает с учетом динамики рынка, новых на нем тенденций. Существующие сегодня криптоботы не способны так быстро адаптироваться под современные требования, торгуют по установленным настройкам.</p>
-            <p >7. Для выбора трейдера есть полная статистика сделок, история торгов.</p>
-            <p >Сотрудничество с нашим сервисом по криптотрейдингу позволит вам получать только проверенные сигналы для сделок, организовать постоянный пассивный доход, изменить жизнь к лучшему.</p>
+
+
+        <section id="statistics" className="counter-show services section-bg">
+          <div className="container aos-init aos-animate" data-aos="fade-up">
+            <div className="row">
+
+              <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-1 mb-1 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+                <div className="icon-box" >
+                  <div className="icon">
+                    {/* <i className="fa fa-users"></i> */}
+                    <span>Icon</span>
+                    <span className="float-right count-number" data-value="549">549</span>
+                  </div>
+                  <a href="" >Активных пользователей</a>
+                </div>
+              </div>
+
+              <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-1 mb-1 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+                <div className="icon-box" >
+                  <div className="icon">
+                    <span>Icon</span>
+                    {/* <i className="fa fa-check"></i> */}
+                    <span className="float-right count-number" data-value="864">864</span>
+                  </div>
+                  <a href="" >Сигналов за 24 часа</a>
+                </div>
+              </div>
+              <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-1 mb-1 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+                <div className="icon-box" >
+                  <div className="icon">
+                    {/* <i className="fa fa-user-plus"></i> */}
+                    <span>Icon</span>
+                    <span className="float-right count-number" data-value="51">51</span>
+                  </div>
+                  <a href="" >Пользователей за 24 часа</a>
+                </div>
+              </div>
+              <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-1 mb-1 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+                <div className="icon-box" >
+                  <div className="icon">
+                    {/* <i className="fa fa-share-alt"></i> */}
+                    <span>Icon</span>
+                    <span className="float-right count-number" data-value="72">72</span>
+                  </div>
+                  <a href="" >Активных трейдеров</a>
+                </div>
+              </div>
+            </div>
           </div>
+        </section>
 
-        </div>
-      </section >
+
+
+
+        <section id="adventage" className="team section-bg">
+          <div className="container aos-init aos-animate" data-aos="fade-up">
+
+            <div className="section-title">
+              <h2>Наши преимущества</h2>
+            </div>
+
+            <div className="row">
+              <div className="col-lg-6 mt-4">
+                <div className="member d-flex align-items-start aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+                  <div className="pic">
+                    <img src="https://strg.copytrade.pro/source/1/1CNvqvXK8zykUhx3EA2XSXgVIF2VLspL.png" className="img-fluid" alt="" />
+                  </div>
+
+                  <div className="member-info">
+                    <h4>Полная статистика</h4>
+                    <span></span>
+                    <p>История сделок трейдера. Статистика по прибыли. Количество подписчиков. Количество продаж тарифов.</p>
+                  </div>
+
+                </div>
+              </div>
+              <div className="col-lg-6 mt-4">
+                <div className="member d-flex align-items-start aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+                  <div className="pic">
+                    <img src="https://strg.copytrade.pro/source/1/Ru4LmFGrU1ZzBb1Ks5eo_fFzfoNoGDbw.png" className="img-fluid" alt="" />
+                  </div>
+
+                  <div className="member-info">
+                    <h4>Автоматическое копирование сигналов 24 часа в сутки</h4>
+                    <span></span>
+                    <p>Трейдеры, следуя выбранному алгоритму и стратегии, выставляет на биржу ордеры и наш сервис отслеживает и копирует 24 часа в сутки подписчикам. Вам не нужно постоянно следить за графиком и анализировать рынок.</p>
+                  </div>
+
+                </div>
+              </div>
+              <div className="col-lg-6 mt-4">
+                <div className="member d-flex align-items-start aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+                  <div className="pic">
+                    <img src="https://strg.copytrade.pro/source/1/bFLUdl2BQWPnQ5-agKg254tgzRlnRotm.png" className="img-fluid" alt="" />
+                  </div>
+
+                  <div className="member-info">
+                    <h4>Маркетплейс</h4>
+                    <span></span>
+                    <p>Покупайте и продавайте сигналы и зарабатывайте вместе с нами.</p>
+                  </div>
+
+                </div>
+              </div>
+              <div className="col-lg-6 mt-4">
+                <div className="member d-flex align-items-start aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+                  <div className="pic">
+                    <img src="https://strg.copytrade.pro/source/1/IS9pbs43yThMj_JJwS2tvK_PXy9-wr2D.png" className="img-fluid" alt="" />
+                  </div>
+
+                  <div className="member-info">
+                    <h4>Широкие возможности сервиса</h4>
+                    <span></span>
+                    <p>С помощью удобного интерфейса можно легко выбрать нужного трейдера и начать зарабатывать, а трейдеры легко могут продавать свои сигналы и получить пассивный доход.</p>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+        <section id="top-traders" className="top-traders section-bg">
+
+          <div className="container">
+            <div className="section-title">
+              <h2>Лучшие трейдеры</h2>
+            </div>
+
+            <div className="row aos-init aos-animate d-flex justify-content-center" data-aos="zoom-in">
+
+              <div className="col-lg-10 col-md-10 col-12 pb-2 flex-center">
+                <div className="card card-body">
+                  <div className="row ">
+                    <div className="col-md-2 top-trader-photo">
+                      {/* <img className="shadow-lg rounded p-2 text-center" src="https://strg.copytrade.pro/source/1/M_YNhM28Vm3rILYvSiAK4n15J-oP9sxV.png" alt="" />                                  */}
+                    </div>
+                    <div className="col-md-4">
+                      <div className="trader-name" >
+                        zafarjonovich
+                      </div>
+                      <div className="trader-description">
+                        еукиууам
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div>
+                        <i className="fas fa-users"></i> Подписки: 0
+                      </div>
+                      <div>
+                        <i className="fas fa-coins"></i> Прибыль: 0.00%
+                      </div>
+                      <div>
+                        <i className="fab fa-sellsy"></i> Продажи: 0
+                      </div>
+
+                    </div>
+                    <div className="col-md-2 pt-2 align-card">
+                      <a className="btn btn-info align-text-bottom" href="/user/marketplace?user_id=4" ><i className="fas fa-eye"></i> Посмотреть</a>                                        </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-10 col-md-10 col-12 pb-2 flex-center">
+                <div className="card card-body">
+                  <div className="row ">
+                    <div className="col-md-2 top-trader-photo">
+                      {/* <img className="shadow-lg rounded p-2 text-center" src="https://strg.copytrade.pro/assets/img/other/anonymous.png" alt="" /> */}
+                    </div>
+                    <div className="col-md-4">
+                      <div className="trader-name" >
+                        webmaster
+                      </div>
+                      <div className="trader-description">
+                        Тестстстстстс
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div>
+                        <i className="fas fa-users"></i> Подписки: 0
+                      </div>
+                      <div>
+                        <i className="fas fa-coins"></i> Прибыль: 0.00%
+                      </div>
+                      <div>
+                        <i className="fab fa-sellsy"></i> Продажи: 1
+                      </div>
+
+                    </div>
+                    <div className="col-md-2 pt-2 align-card">
+                      <a className="btn btn-info align-text-bottom" href="/user/marketplace?user_id=1" ><i className="fas fa-eye"></i> Посмотреть</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+        </section>
+
+      </main>
+
+
 
     </>
   )
