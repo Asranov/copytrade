31d756b74f4db73ae5f28212710791c60d955137
Asranov <asranov0003@gmail.com>
2023-03-12 18:31:25 +0500

    router dom added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Main from './Pages/Main/Main';
+import Signin from './Routers/Auth/Signin';
 
 function App() {
   return (
     <>
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="signin" element={<Signin />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   );
 }
